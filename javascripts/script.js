2fa8a2098aed9982641d3a3ea1a04171c32e54d2
Tabrez Khana <khan.t.29@gmail.com>
2018-04-05 00:03:18 +0530

    intial commit

---
+++ javascripts/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    
+    //smoothscroll
+    $('.sidebar a[href^="#"], .about a[href^="#"]').on('click', function (e) {
+        // e.preventDefault();
+        $(document).off("scroll");
+        
+        $('.sidebar a').each(function () {
+            $(this).removeClass('active-menu');
+        });
+        $(this).addClass('active-menu');
+        $('.about a').removeClass('active-menu');
+        $('.sidebar-social a').removeClass('active-menu');
+      
+        var hash = this.hash;
+        $('html, body').stop().animate({
+            'scrollTop': $(hash).offset().top+2
+        }, 300, 'swing', function () {
+            window.location.hash = hash;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    var sections = $('section');
+    $(window).on('scroll', function() {
+        var currentPosition = $(this).scrollTop();
+
+        sections.each(function(){
+            var top = $(this).offset().top;
+            var bottom = top + $(this).outerHeight();
+            if(currentPosition >= top && currentPosition <= bottom) {
+                $('.sidebar').find('li').removeClass('active-menu');
+                $('.sidebar').find('a[href="#'+$(this).attr('id')+'"]').addClass('active-menu');
+            }
+            else {
+                $('.sidebar').find('li').removeClass('active-menu');
+                $('.sidebar').find('a[href="#'+$(this).attr('id')+'"]').removeClass('active-menu');
+            }
+        });
+    });
+
+    $('.menu-button').on('click', function () {
+        $('.sidebar').addClass('active-sidebar');
+        $('.my-overlay').fadeIn();
+    });
+
+    $('.my-overlay').on('click', function () {
+        $('.sidebar').removeClass('active-sidebar');
+        $('.my-overlay').fadeOut();
+    });
+
+    $(".swipe-area").swipe({
+        swipeStatus:function(event, phase, direction, distance, duration, fingers)
+        {
+            if (phase=="move" && direction =="right") {
+                $('.sidebar').addClass('active-sidebar');
+                $('.my-overlay').fadeIn();
+                return false;
+            }
+            if (phase=="move" && direction =="left") {
+                $('.sidebar').removeClass('active-sidebar');
+                $('.my-overlay').fadeOut();
+                return false;
+            }
+        }, allowPageScroll: "auto"
+    });
+
+    $(".sidebar").swipe({
+        swipeStatus:function(event, phase, direction, distance, duration, fingers)
+        {
+            
+            if (phase=="move" && direction =="left") {
+                $('.sidebar').removeClass('active-sidebar');
+                $('.my-overlay').fadeOut();
+                return false;
+            }
+        }, allowPageScroll: "auto"
+    });
+});
